ed3783d7a1d5b5344788855e8a09be4a1025ec7c
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-03-21 11:26:30 -0700

    Created node class

---
+++ FindHeight/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindHeight
+{
+   public class Node
+    {
+        public Node RightChild { get; set; }
+        public Node LeftChild { get; set; }
+        public int Value { get; set; }
+
+    }
+}
