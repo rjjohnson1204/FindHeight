fac78c5da10cd71ee254221c6605e49fd0b52232
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-03-21 11:21:27 -0700

    Add project files.

---
+++ FindHeight/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindHeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
